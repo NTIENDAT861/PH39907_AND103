7e092c150f8895418d0cd76cb2928d66c05cc666
NTIENDAT <dattk305g@gmail.com>
2024-04-04 14:56:26 +0700

    lab6_app_p2

--- PH39907_AND103/Lab6/app/src/main/java/com/example/lab6/adapter/FruitAdapter.java
+++ PH39907_AND103/Lab6/app/src/main/java/com/example/lab6/adapter/FruitAdapter.java
@@ -17,7 +17,7 @@ import com.example.lab6.model.Fruit;
 
 import java.util.ArrayList;
 
-public class FruitAdapter  extends RecyclerView.Adapter<FruitAdapter.ViewHolder>{
+public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> {
     private Context context;
     private ArrayList<Fruit> list;
     private FruitClick fruitClick;
@@ -30,13 +30,14 @@ public class FruitAdapter  extends RecyclerView.Adapter<FruitAdapter.ViewHolder>
 
     public interface FruitClick {
         void delete(Fruit fruit);
+
         void edit(Fruit fruit);
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        ItemFruitBinding binding = ItemFruitBinding.inflate(LayoutInflater.from(parent.getContext()),parent,false);
+        ItemFruitBinding binding = ItemFruitBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
 
         return new ViewHolder(binding);
     }
@@ -45,15 +46,25 @@ public class FruitAdapter  extends RecyclerView.Adapter<FruitAdapter.ViewHolder>
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Fruit fruit = list.get(position);
         holder.binding.tvName.setText(fruit.getName());
-        holder.binding.tvPriceQuantity.setText("price :" +fruit.getPrice()+" - quantity:" +fruit.getQuantity());
+        holder.binding.tvPriceQuantity.setText("price :" + fruit.getPrice() + " - quantity:" + fruit.getQuantity());
         holder.binding.tvDes.setText(fruit.getDescription());
-        String url  = fruit.getImage().get(0);
+        String url = fruit.getImage().get(0);
         String newUrl = url.replace("localhost", "10.0.2.2");
+
+
+        holder.binding.btnDelete.setOnClickListener(v -> {
+            fruitClick.delete(fruit);
+        });
+
+        holder.binding.btnEdit.setOnClickListener(v -> {
+            fruitClick.edit(fruit);
+        });
+
         Glide.with(context)
                 .load(newUrl)
                 .thumbnail(Glide.with(context).load(R.drawable.baseline_broken_image_24))
                 .into(holder.binding.img);
-        Log.d("321321", "onBindViewHolder: "+list.get(position).getImage().get(0));
+        Log.d("321321", "onBindViewHolder: " + list.get(position).getImage().get(0));
     }
 
 
@@ -64,6 +75,7 @@ public class FruitAdapter  extends RecyclerView.Adapter<FruitAdapter.ViewHolder>
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         ItemFruitBinding binding;
+
         public ViewHolder(ItemFruitBinding binding) {
             super(binding.getRoot());
             this.binding = binding;
--- PH39907_AND103/Lab6/app/src/main/java/com/example/lab6/view/HomeActivity.java
+++ PH39907_AND103/Lab6/app/src/main/java/com/example/lab6/view/HomeActivity.java
@@ -1,26 +1,36 @@
 package com.example.lab6.view;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
+import com.bumptech.glide.Glide;
 import com.example.lab6.R;
 import com.example.lab6.adapter.FruitAdapter;
 import com.example.lab6.databinding.ActivityHomeBinding;
+import com.example.lab6.databinding.DialogEditFruitBinding;
+import com.example.lab6.model.Distributor;
 import com.example.lab6.model.Fruit;
 import com.example.lab6.model.Response;
 import com.example.lab6.services.HttpRequest;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 
@@ -30,13 +40,17 @@ public class HomeActivity extends AppCompatActivity implements FruitAdapter.Frui
     private SharedPreferences sharedPreferences;
     private String token;
     private FruitAdapter adapter;
-
+    private String id_Distributor;
+    private ArrayList<Distributor> distributorArrayList;
+    private ArrayList<File> ds_image;
+    private  DialogEditFruitBinding binding1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
         setContentView(binding.getRoot());
         httpRequest = new HttpRequest();
+        ds_image = new ArrayList<>();
         sharedPreferences = getSharedPreferences("INFO",MODE_PRIVATE);
 
         token = sharedPreferences.getString("token","");
@@ -78,17 +92,148 @@ public class HomeActivity extends AppCompatActivity implements FruitAdapter.Frui
 
     @Override
     public void delete(Fruit fruit) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Confirm delete");
+        builder.setMessage("Are you sure you want to delete?");
+        builder.setPositiveButton("yes", (dialog, which) -> {
 
+            httpRequest.callAPI().deleteFruits(fruit.get_id()).enqueue(responseFruitAPI);
+        });
+        builder.setNegativeButton("no", (dialog, which) -> {
+            dialog.dismiss();
+        });
+        builder.show();
     }
 
+    Callback<Response<Fruit>> responseFruitAPI  = new Callback<Response<Fruit>>() {
+        @Override
+        public void onResponse(Call<Response<Fruit>> call, retrofit2.Response<Response<Fruit>> response) {
+            if (response.isSuccessful()) {
+                if (response.body().getStatus() == 200) {
+                    httpRequest.callAPI().getListFruit("Bearer " + token).enqueue(getListFruitResponse);
+                    Toast.makeText(HomeActivity.this, response.body().getMessenger(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+
+        @Override
+        public void onFailure(Call<Response<Fruit>> call, Throwable t) {
+
+        }
+    };
     @Override
     public void edit(Fruit fruit) {
-
+        showDialogEdit(fruit);
     }
+    private void showDialogEdit(Fruit fruit) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Edit distributor");
+         binding1 = DialogEditFruitBinding.inflate(LayoutInflater.from(this));
+        builder.setView(binding1.getRoot());
+        AlertDialog alertDialog = builder.create();
+        configSpinner(fruit.getDistributor());
 
+        String url = fruit.getImage().get(0);
+        String newUrl = url.replace("localhost", "10.0.2.2");
+
+        binding1.edName.setText(fruit.getName());
+        binding1.edDescription.setText(fruit.getDescription());
+        binding1.edPrice.setText(fruit.getPrice());
+        binding1.edQuantity.setText(fruit.getQuantity());
+        binding1.edStatus.setText(fruit.getStatus());
+
+        Glide.with(this)
+                .load(newUrl)
+                .thumbnail(Glide.with(this).load(R.drawable.baseline_broken_image_24))
+                .into(binding1.avatar);
+        binding1.btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Map<String , RequestBody> mapRequestBody = new HashMap<>();
+                String _name = binding1.edName.getText().toString().trim();
+                String _quantity = binding1.edQuantity.getText().toString().trim();
+                String _price = binding1.edPrice.getText().toString().trim();
+                String _status = binding1.edStatus.getText().toString().trim();
+                String _description = binding1.edDescription.getText().toString().trim();
+
+
+
+                mapRequestBody.put("name", getRequestBody(_name));
+                mapRequestBody.put("quantity", getRequestBody(_quantity));
+                mapRequestBody.put("price", getRequestBody(_price));
+                mapRequestBody.put("status", getRequestBody(_status));
+                mapRequestBody.put("description", getRequestBody(_description));
+                mapRequestBody.put("id_distributor", getRequestBody(id_Distributor));
+                ArrayList<MultipartBody.Part> _ds_image = new ArrayList<>();
+
+                ds_image.forEach(file1 -> {
+                    RequestBody requestFile = RequestBody.create(MediaType.parse("image/*"),file1);
+                    MultipartBody.Part multipartBodyPart = MultipartBody.Part.createFormData("image", file1.getName(),requestFile);
+                    _ds_image.add(multipartBodyPart);
+                });
+                if (_name.isEmpty() || _quantity.isEmpty() || _price.isEmpty() || _status.isEmpty() ) {
+                    Toast.makeText(HomeActivity.this, "Enter data", Toast.LENGTH_SHORT).show();
+                }   else {
+
+                    httpRequest.callAPI().updateFruitWithFileImage(mapRequestBody,fruit.get_id(), _ds_image).enqueue(responseFruitAPI);
+                    alertDialog.dismiss();
+                }
+            }
+        });
+        binding1.btnCancle.setOnClickListener(v -> {
+            alertDialog.dismiss();
+        });
+        alertDialog.show();
+    }
+    private RequestBody getRequestBody(String value) {
+        return RequestBody.create(MediaType.parse("multipart/form-data"),value);
+    }
     @Override
     protected void onResume() {
         super.onResume();
         httpRequest.callAPI().getListFruit("Bearer "+token).enqueue(getListFruitResponse);
     }
+    private void configSpinner(Distributor distributor) {
+        httpRequest.callAPI().getListDistributor().enqueue(getDistributorAPI);
+        binding1.spDistributor.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+
+                id_Distributor = distributorArrayList.get(position).getId();
+                Log.d("123123", "onItemSelected: " + id_Distributor);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+
+    Callback<Response<ArrayList<Distributor>>> getDistributorAPI = new Callback<Response<ArrayList<Distributor>>>() {
+        @Override
+        public void onResponse(Call<Response<ArrayList<Distributor>>> call, retrofit2.Response<Response<ArrayList<Distributor>>> response) {
+            if (response.isSuccessful()) {
+                if (response.body().getStatus() == 200) {
+                    distributorArrayList = response.body().getData();
+                    String[] items = new String[distributorArrayList.size()];
+
+                    for (int i = 0; i< distributorArrayList.size(); i++) {
+                        items[i] = distributorArrayList.get(i).getName();
+                    }
+                    ArrayAdapter<String> adapterSpin = new ArrayAdapter<>(HomeActivity.this, android.R.layout.simple_spinner_item, items);
+                    adapterSpin.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    binding1.spDistributor.setAdapter(adapterSpin);
+                }
+            }
+        }
+
+        @Override
+        public void onFailure(Call<Response<ArrayList<Distributor>>> call, Throwable t) {
+            t.getMessage();
+        }
+
+    };
 }
